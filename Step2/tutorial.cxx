208821fa3431b65ef8b85f714df5c1d6b65bdd45
ron <dev_ron@163.com>
2019-12-12 16:42:01 +0800

    Step2

--- Step2/tutorial.cxx
+++ Step2/tutorial.cxx
@@ -4,23 +4,37 @@
 #include <string>
 
 #include "TutorialConfig.h"
+// #ifdef USE_MYMATH
+// for #cmakedefine01 USE_MYMATH
+#if defined(USE_MYMATH) && (USE_MYMATH == 1) 
+#  include "MathFunctions.h"
+#endif
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
 
-  // convert input to double
-  const double inputValue = std::stod(argv[1]);
+    // convert input to double
+    const double inputValue = std::stod(argv[1]);
 
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+    // calculate square root
+    // const double outputValue = sqrt(inputValue);
+    
+// #ifdef USE_MYMATH
+// for #cmakedefine01 USE_MYMATH
+#if defined(USE_MYMATH) && (USE_MYMATH == 1) 
+    const double outputValue = mysqrt(inputValue);
+#else
+    const double outputValue = sqrt(inputValue);
+#endif
+
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+                << std::endl;
+    return 0;
 }
