52afce3984e7e4f612898a52fb71cf7036b7490a
Ron2014 <dev_ron@163.com>
2021-02-19 10:21:51 +0800

    Step9 tutorial

--- Step9/tutorial.cxx
+++ Step9/tutorial.cxx
@@ -1,8 +1,14 @@
 // A simple program that computes the square root of a number
+#include <cmath>
 #include <iostream>
 #include <string>
 
 #include "TutorialConfig.h"
+
+// should we include the MathFunctions header?
+// #ifdef USE_MYMATH
+// #  include "MathFunctions.h"
+// #endif
 #include "MathFunctions.h"
 
 int main(int argc, char* argv[])
@@ -17,8 +23,15 @@ int main(int argc, char* argv[])
 
   // convert input to double
   const double inputValue = std::stod(argv[1]);
-  const double outputValue = mathfunctions::sqrt(inputValue);
 
+  // which square root function should we use?
+// #ifdef USE_MYMATH
+//   const double outputValue = mysqrt(inputValue);
+// #else
+//   const double outputValue = sqrt(inputValue);
+// #endif
+
+  const double outputValue = sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
